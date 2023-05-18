06019d44d1102dc79a29cba48c8dc3311035a924
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-05-18 21:48:59 +0100

    running

--- src/com/femmycode/solidPrinciples/Main.java
+++ src/com/femmycode/solidPrinciples/Main.java
@@ -1,19 +1,18 @@
 package com.femmycode.solidPrinciples;
 
 import java.util.List;
-import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 
         AreaCalculator areaCalculator = new AreaCalculator();
-        Circle circle = new Circle();
-        Square square = new Square();
+        Circle circle = new Circle(10);
+        Square square = new Square(10);
 
-        List<Objects> shapes = List.of(circle, square);
-        int sum = areaCalculator.sum(shapes);
-        System.out.println("Sum = " + sum);
+        List<Object> shapes = List.of(circle, square);
+        int sum1 = areaCalculator.sum(shapes);
+        System.out.println("Sum = " + sum1);
 
     }
 }
